824d47a167b95f1b5ce91730dcce650eb092ab42
Harald Heide Gundersen <harald.heide.gundersen@hotmail.com>
2018-06-20 18:46:20 +0200

    Create GazePointer.cs

---
+++ GazePointer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class GazePointer : MonoBehaviour
+{
+    GameObject hitObject;
+    RaycastHit hit;
+    public Reticle reticle;
+
+	// Update is called once per frame
+	void Update () {
+	    if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            reticle.SetPosition(hit);
+            hitObject = hit.transform.gameObject;
+            Interactable interactable = hitObject.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                interactable.targetted = true;
+            }
+        } else
+        {
+            reticle.SetPosition();
+        }
+	}
+}
